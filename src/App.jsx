7e56de54e22a75b09eb873dc93b57623cd962b52
Aurelio D'Urso <71693468+aureliodd@users.noreply.github.com>
2023-09-15 14:33:10 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import './App.css'
+import { useEffect, useState } from 'react'
+
+import me from './assets/me.jpg'
+import instagram from './assets/instagram.png'
+import linkedin from './assets/linkedin.png'
+import github from './assets/github.png'
+
+const QUALITIES = [
+  'Web Developer',
+  'App Developer'
+]
+
+function App() {
+
+  // const [scrollPosition, setScrollPosition] = useState(0);
+  const [qualityIndex, setQualityIndex] = useState(0);
+
+  const currentYear = 1900 + new Date().getYear() 
+
+  // useEffect(() => {
+  //   window.addEventListener('scroll', handleScroll, { passive: true });
+  //   return () => window.removeEventListener('scroll', handleScroll);
+  // }, []);
+
+  // function handleScroll() {
+  //   setScrollPosition(window.pageYOffset)
+  // }
+
+  return (
+    <div className="container">
+
+      <header> {/* Empty. For future use? */} </header>
+      
+
+      <section>
+        {/* <div className="bubble grow left">
+
+        </div> */}
+
+        <img className="myPic" src={me} />
+        <div className="myName">
+          <p>AURELIO DURSO</p>
+        </div>
+        <p>{QUALITIES[qualityIndex]}</p>
+
+        <div className="social">
+          <a href="http://github.com/aureliodd" rel='noreferrer' target='_blank'>
+            <img src={github} />
+          </a>
+          <a href="https://www.linkedin.com/in/aurelio-d-urso-92a680161/" rel='noreferrer' target='_blank'>
+            <img src={linkedin} /> 
+          </a>
+          <a href="http://instagram.com/aureliodd" rel='noreferrer' target='_blank'>
+            <img src={instagram} /> 
+          </a>
+        </div>
+      </section>
+
+      <footer>
+        aureliodurso.com © {currentYear}. All rights reserved.
+      </footer>
+      
+    </div>
+  )
+}
+
+export default App
