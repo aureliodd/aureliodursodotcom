ac343ff63801ecad54dbf3cae5ae0b351018109e
Aurelio D'Urso <71693468+aureliodd@users.noreply.github.com>
2024-01-05 11:06:02 +0100

    resolved bug with dates in between years

--- src/classes/utils.jsx
+++ src/classes/utils.jsx
@@ -4,22 +4,37 @@ import { SEASONS } from '../constants/constants.js'
 export const getPeriodOfTheYear = () => {
 
   const today = new Date()
-  // const today = new Date('Mar 21 2023') // Spring
-  // const today = new Date('Jun 21 2023') // Summer
-  // const today = new Date('Sep 21 2023') // Autumn
-  // const today = new Date('Dec 21 2023') // Winter
-  // const today = new Date('Dec 25 2023') // Christmas
 
   const currentYear = today.getFullYear()
 
   const seasons = SEASONS.map(element => {
-    let fromCurrentYearToNext = element.start[1] > element.end[1]
+    let betweenTwoYears = element.start[1] > element.end[1]
+
+    let startYear, endYear
+    startYear = endYear = currentYear
+
+    if(betweenTwoYears) {
+      let newYears = new Date(currentYear, 0, 1) // 1 gennaio dell'anno corrente
+      let endDate = new Date(currentYear, element.start[1] - 1, element.start[0])
+
+      
+      if(newYears < endDate) {
+        //caso siamo nell'anno nuovo
+        startYear--
+      } else {
+        // caso siamo nell'anno vecchio
+        endYear++
+      }
+    } 
+
     return {
       name: element.name,
-      start: new Date(currentYear, element.start[1] - 1, element.start[0]),
-      end: new Date(fromCurrentYearToNext ? currentYear + 1 : currentYear, element.end[1] - 1, element.end[0])
+      start: new Date(startYear, element.start[1] - 1, element.start[0]),
+      end: new Date(endYear, element.end[1] - 1, element.end[0])
     };
   })
+
+  console.log(seasons)
 
   let classN = []
 
